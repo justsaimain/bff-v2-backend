dcf5fb202da06daa2e2612dc5ff62512367dd3a2
justsaimain <saimain.primary@gmail.com>
2022-02-01 01:14:54 +0630

    user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Admin;
 use App\Models\Option;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => "Test User",
+            "phone" => "123",
+            'phone_verified_at' => now(),
+            'email' => 'test@gmail.com',
+            'email_verified_at' => now(),
+            'region' => "Yangon",
+            'fav_team' => 'West Ham',
+            'password' => '123',
+            'remember_token' => Str::random(10),
+        ]);
+
         \App\Models\User::factory(100)->create();
 
         Option::create([
