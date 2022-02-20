641dcb2bfac175b5bb9c2a46a5aea5f44c515a65
Sai Main <saimain.primary@gmail.com>
2022-02-20 11:42:16 +0630

    prediction finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,19 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => "Test User",
-            "phone" => "123",
-            'phone_verified_at' => now(),
-            'email' => 'test@gmail.com',
-            'email_verified_at' => now(),
-            'region' => "Yangon",
-            'fav_team' => 1,
-            'password' => '123',
-            'remember_token' => Str::random(10),
-        ]);
+        // User::create([
+        //     'name' => "Test User",
+        //     "phone" => "123",
+        //     'phone_verified_at' => now(),
+        //     'email' => 'test@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'region' => "Yangon",
+        //     'fav_team' => 1,
+        //     'password' => '123',
+        //     'remember_token' => Str::random(10),
+        // ]);
 
-        \App\Models\User::factory(100)->create();
+        // \App\Models\User::factory(100)->create();
 
         Option::create([
             'current_gameweek' => 24
