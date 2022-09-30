8b9fc567f938b0976a2022068e2a2c324481d3b7
Sai Main <saimain.venuslab@gmail.com>
2022-09-30 22:51:58 +0630

    leaderboard detail

--- app/Http/Controllers/API/LeaderboardController.php
+++ app/Http/Controllers/API/LeaderboardController.php
@@ -177,18 +177,12 @@ class LeaderboardController extends Controller
 
 
         $result = array();
-
-
         foreach ($arrayData as $k => $v) {
-                $id = $v['user']['id'];
-                if($id){
-                    $result[$id]['pts'][] = $v['total_pts'];
-                    $result[$id]['point_logs'][strval($v['team_h']) . " vs " . strval($v['team_a']) ] = $v['point_logs'];
-                    $result[$id]['fixture_logs'][strval($v['team_h']) . " vs " . strval($v['team_a']) ] = $v['fixture_logs'];
-                    $result[$id]['user'] = $v['user'];
-                }
-
-            }
+            $id = $v['user']['id'];
+            $result[$id]['pts'][] = $v['total_pts'];
+            $result[$id]['point_logs'][strval($v['team_h']) . " vs " . strval($v['team_a']) ] = $v['point_logs'];
+            $result[$id]['fixture_logs'][strval($v['team_h']) . " vs " . strval($v['team_a']) ] = $v['fixture_logs'];
+            $result[$id]['user'] = $v['user'];
         }
 
         $new = array();
